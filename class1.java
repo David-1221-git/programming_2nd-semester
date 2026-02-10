5c8b868ce4c4bff146aa35aa97653bf87a4b8671
David-1221-git <merinodominguezdavid@gmail.com>
2026-02-10 09:16:10 -0600

    update code class 1

--- class1.java
+++ class1.java
@@ -1,6 +1,6 @@
 public class class1 {
     public static void main(String[] args) {
-        float num=3.1416f;
-        System.out.printf("pi es %.4f%n",num);
+        float pi=3.1416f;
+        System.out.printf("pi es %.4f%n",pi);
     }
 }
