a50e880a0530c80949e06e6b72a6609230b8ce11
David-1221-git <merinodominguezdavid@gmail.com>
2026-05-07 09:27:02 -0600

    Refactor code structure for improved readability and maintainability

---
+++ U5A1/Try1.java
@@ -0,0 +1,9 @@
+package U5A1;
+
+public class Try1{
+    public static void main(String arg[]){
+        int [] array = new int[20];
+		array[-3] = 24;	
+	    }
+}
+         
---
+++ U5A1/Try5.java
@@ -0,0 +1,12 @@
+package U5A1;
+
+public class Try5 {
+    public static void main(String arg[]) {
+        try {
+            Exception e = new Exception("Este es mi propio error.");
+            throw e;
+        } catch (Exception excepcion) {
+            excepcion.printStackTrace();
+        }
+    }
+}
